ae985bc2939b322ef3f4fd78dd92fb2d60af853c
nsiramsetty <naresh.siramsetty@tafensw.edu.au>
2019-10-21 00:33:13 +1100

    Final Commit :-)

--- test/unit/specs/Home.spec.js
+++ test/unit/specs/Home.spec.js
@@ -1,7 +1,5 @@
 // Just Implemented a Framework for Test,
 // Ideal Test cases were not implemented due to time limit.
-import Vue from 'Vue';
-import Home from '../../../src/_view/public/routes/Home/Home.vue.js';
 describe('Archistar AI - Vue JS - Coding Test', () => {
 
 });
